201ea3815614897d25eddbf4283766a1037b5c12
minalraval <minal@hyvikk.com>
2022-11-29 10:19:26 +0530

    admin delete product module added

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,6 +9,14 @@ use App\Models\Product;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+    public $product_id;
+    public function deleteProduct()
+    {
+        $product=Product::find($this->product_id);
+        unlink('assets/imgs/products/' .$product->image);
+        $product->delete();
+        session()->flash('message','The product has been deleted!');
+    }
     public function render()
     {
         $products=Product::orderBy('created_at','DESC')->paginate(10);
