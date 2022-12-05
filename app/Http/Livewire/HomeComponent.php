343153e9a0bc4ae1ed26fc2b20835da165b7189a
minalraval <minal@hyvikk.com>
2022-12-05 17:14:12 +0530

    dynamic featured products and new products added at homepage

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -4,12 +4,24 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\HomeSlider;
+use App\Models\Product;
+use Cart;
 
 class HomeComponent extends Component
 {
+    public function store($product_id,$product_name,$product_price)
+    {
+        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        session()->flash('success_message','Item added in Cart');
+        $this->emitTo('cart-icon-component','refreshComponent');
+        return redirect()->route('shop.cart');
+    }
+
     public function render()
     {
         $slides=HomeSlider::where('status',1)->get();
-        return view('livewire.home-component',['slides'=>$slides]);
+        $lproducts=Product::orderBy('created_at','DESC')->get()->take(8);
+        $fproducts=Product::where('featured',1)->inRandomOrder()->get()->take(8);
+        return view('livewire.home-component',['slides'=>$slides,'lproducts'=>$lproducts,'fproducts'=>$fproducts]);
     }
 }
